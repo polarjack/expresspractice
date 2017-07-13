afbd5811ba3b404c42cecc6856cf5631adbc8d45
polarjack <luckyjack85@gmail.com>
2017-07-14 02:45:08 +0800

    sectioin 18

--- script13.js
+++ script13.js
@@ -1,10 +1,27 @@
-var express = require('express')
+var express = require('express');
+var path = require('path')
+var fs = require('fs')
+var app = express();
 
-var app = express()
 
-app.get('/helloThere', function(require, response) {
-  response.send("hello there, from express")
-});
+app.use('/cssFile', express.static(__dirname + '/assets'))
+
+app.get('/', function(req, res) {
+  res.sendfile('index.html', {root: path.join(__dirname, './files')})
+})
+
+app.get(/^(.+)$/, function(req, res) {
+  console.log(path.join(__dirname, './files/', req.params[0]+'.html'));
+  try{
+    if(fs.statSync(path.join(__dirname, './files/', req.params[0])).isFile()) {
+      res.sendfile(req.params[0] + '.html', {root: path.join(__dirname, './files')})
+    }
+  }catch(err) {
+    res.sendfile('404.html', {root: path.join(__dirname, './files')})
+  }
+  
+})
+
 
 app.listen(5000, function() {
   console.log('Listening at Port 5000')
